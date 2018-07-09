cc0eb9a5d92efc26a35a3f11f4e2230eb148ea7d
Laurence <40289918+protodroid-IB@users.noreply.github.com>
2018-07-09 18:10:15 +0800

    Initial commit

---
+++ Assets/Scripts/TorchColor.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchColor : MonoBehaviour
+{
+    // the colors to use for the torches
+    [SerializeField] private Color redFlame, orangeFlame, blueFlame, greenFlame, yellowFlame, purpleFlame;
+
+    // possible states for the current colour of the torch
+    private enum ColorState {Red, Orange, Blue, Green, Yellow, Purple};
+
+    // current color state
+    private ColorState currentColorState = ColorState.Orange;
+
+    // mesh renderer of the flame gameobject
+    [SerializeField] private MeshRenderer flameMR;
+
+
+    private void Start()
+    {
+        CollideWithBullet();
+    }
+
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+        ColorStateSwitch();
+    }
+
+
+
+
+    // this methods switches between torches color states
+    private void ColorStateSwitch()
+    {
+        switch (currentColorState)
+        {
+            case ColorState.Red:
+                ChangeColor(redFlame);
+                break;
+
+            case ColorState.Orange:
+                ChangeColor(orangeFlame);
+                break;
+
+            case ColorState.Blue:
+                ChangeColor(blueFlame);
+                break;
+
+            case ColorState.Green:
+                ChangeColor(greenFlame);
+                break;
+
+            case ColorState.Yellow:
+                ChangeColor(yellowFlame);
+                break;
+
+            case ColorState.Purple:
+                ChangeColor(purpleFlame);
+                break;
+
+            default:
+                ChangeColor(orangeFlame);
+                break;
+        }
+    }
+
+
+
+
+
+    // this method changes the color of the torch
+    public void ChangeColor(Color inColor)
+    {
+        // implement color changing behaviour here
+        flameMR.material.color = inColor;
+
+    }
+
+
+
+
+    public void CollideWithBullet()
+    {
+        currentColorState = ColorState.Green;
+        // implement bullet collision behaviour here
+    }
+}
---
+++ Assets/Scripts/TorchSwitch.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchSwitch : MonoBehaviour
+{
+    // boolean that determines if torch is on
+    [SerializeField]
+    private bool torchOn = false;
+
+    // the gameobject for the torch's fire particles
+    [SerializeField]
+    private GameObject fireParticlesGO;
+
+
+    private void Start()
+    {
+        if(torchOn == false)
+        {
+            Off();
+        }
+    }
+
+    // turns the torch on
+    public void On()
+    {
+        torchOn = true;
+        fireParticlesGO.SetActive(torchOn);
+    } 
+
+    // turns the torch off
+    public void Off()
+    {
+        torchOn = false;
+        fireParticlesGO.SetActive(torchOn);
+    }
+
+    // grabs the torch state 
+    public bool GetTorchState()
+    {
+        return torchOn;
+    }
+
+
+
+
+    private void TestSwitch()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            if (torchOn == false)
+            {
+                On();
+            }
+            else
+            {
+                Off();
+            }
+        }
+    }
+
+    private void Update()
+    {
+        TestSwitch();
+    }
+}
