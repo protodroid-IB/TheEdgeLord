0d940309af98d6027a9a6250ca9aa44d581647a4
Laurence <40289918+protodroid-IB@users.noreply.github.com>
2018-07-09 19:08:18 +0800

    gun aiming

--- Assets/Scripts/GunAim.cs
+++ Assets/Scripts/GunAim.cs
@@ -4,16 +4,59 @@ using UnityEngine;
 
 public class GunAim : MonoBehaviour
 {
+    [SerializeField]
+    private Transform bulletSpawnTransform;
 
-	// Use this for initialization
-	void Start ()
+    private LineRenderer lineOfSight;
+    private Camera fpsCam;
+
+    private Vector3 fireDirectionPoint;
+
+    private float fireRange = 150f;
+
+
+    private void Start()
     {
-		//test
-	}
+        fpsCam = Camera.main;
+        lineOfSight  = GetComponent<LineRenderer>();
+    }
+
+
+    public Vector3 GetFireDirectionPoint()
+    {
+        return fireDirectionPoint;
+    }
+
+    public Vector3 GetFireDirection()
+    {
+        return (bulletSpawnTransform.position - fireDirectionPoint).normalized;
+    }
 	
+
+
+
 	// Update is called once per frame
-	void Update ()
+	private void Update ()
     {
-		
-	}
+        Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(.5f, .5f, 0));
+
+        RaycastHit hit;
+
+        lineOfSight.SetPosition(0, bulletSpawnTransform.position);
+
+        if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, fireRange))
+        {
+            fireDirectionPoint = hit.point;
+
+            lineOfSight.SetPosition(1, fireDirectionPoint);
+        }
+        else
+        {
+            fireDirectionPoint = rayOrigin + (fpsCam.transform.forward * fireRange);
+
+            lineOfSight.SetPosition(1, fireDirectionPoint);
+        }
+
+    }
+
 }
