95213153d22604943cead295554a0b929095dfe5
Laurence <101215@student.sae.edu.au>
2018-07-12 12:12:20 +0800

    starting floor puzzles

---
+++ Assets/Floor00.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor00 : MonoBehaviour
+{
+    [SerializeField]
+    private DoorSwitch[] doors;
+
+    [SerializeField]
+    private TorchColor[] torchesColor;
+
+    [SerializeField]
+    private TorchSwitch[] torches;
+
+    [SerializeField]
+    private PressureSwitch[] pressureSwitches;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
