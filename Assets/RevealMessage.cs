f4474dc681012245b3b6b6824da1e0ccbc71eb38
Laurence <101215@student.sae.edu.au>
2018-07-14 09:52:50 +0800

    reveal messages with torch and bullets

--- Assets/RevealMessage.cs
+++ Assets/RevealMessage.cs
@@ -15,24 +15,28 @@ public class RevealMessage : MonoBehaviour
 
     private Text message;
 
-    private float closestLightDistance = 1000f;
+    private float messageRevealRate = 1f;
 
-    private float closestBulletLightDistance = 1000f;
+    [SerializeField]
+    private bool messageRevealed = false;
+
 
     private float alpha = 0f;
-    private float originalAlpha = 0f;
 
-    private List<TorchColor> torches;
+
+    private List<TorchColor> torchesColor;
+    private List<TorchSwitch> torchesSwitch;
 
 
 	// Use this for initialization
 	void Start ()
     {
-        torches = new List<TorchColor>();
+        torchesColor = new List<TorchColor>();
+        torchesSwitch = new List<TorchSwitch>();
 
         message = transform.GetChild(0).GetComponent<Text>();
+
         alpha = 0f;
-        originalAlpha = alpha;
 
         Color newColor = new Color(message.color.r, message.color.g, message.color.b, alpha);
 
@@ -41,44 +45,62 @@ public class RevealMessage : MonoBehaviour
 
 
 
-    private void OnTriggerEnter(Collider other)
+
+
+
+    private void Update()
     {
-        // collided with torch range collider
-        if(other.gameObject.tag == "LightRangeCollider")
-        {
-            float distance = Distance(other.transform.position);
+        FadeAlphaToZero();
 
-            // distance is lower than closest torch
-            if(distance < closestLightDistance)
-            {
-                closestLightDistance = distance;
+        RevealFromTorch();
 
-                if (closestLightDistance <= messageRevealRange && closestLightDistance >= 0f)
-                {
+        ClampAlpha();
 
-                    if(other.transform.parent.GetComponent<TorchSwitch>().GetTorchState())
-                    {
-                        TorchColor thisTorch = other.transform.parent.GetComponent<TorchColor>();
+        CheckMessageRevealed();
 
-                        torches.Add(thisTorch);
-
-                        if (thisTorch.GetColorState() == revealWhen.ToString())
-                        {
-                            float ratio = ((messageRevealRange - closestLightDistance) / messageRevealRange);
-
-                            AddToAlpha(ratio);
-                        }
-                    }
-
-                    Color newColor = new Color(message.color.r, message.color.g, message.color.b, alpha);
-
-                    message.color = newColor;
-                }
-            }
-
-            
-        }
+        SetAlpha();
     }
+
+
+
+
+
+
+
+    private void FadeAlphaToZero()
+    {
+        alpha -= messageRevealRate * Time.deltaTime;
+    }
+
+    private void ClampAlpha()
+    {
+        if (alpha >= 1f) alpha = 1f;
+        if (alpha <= 0f) alpha = 0f;
+    }
+
+    private void SetAlpha()
+    {
+        Color newColor = new Color(message.color.r, message.color.g, message.color.b, alpha);
+
+        message.color = newColor;
+    }
+
+
+
+    private void CheckMessageRevealed()
+    {
+        if (alpha >= 0.18f) messageRevealed = true;
+        else messageRevealed = false;
+
+    }
+
+
+
+    public bool Revealed()
+    {
+        return messageRevealed;
+    }
+
 
 
     private float Distance(Vector3 objectPosition)
@@ -97,99 +119,132 @@ public class RevealMessage : MonoBehaviour
         return direction;
     }
 
-    private void Update()
-    {
-        for (int i = 0; i < torches.Count; i++)
-        {
-            bool correctColour = torches[i].GetColorState() == revealWhen.ToString();
 
-            if (correctColour)
-            {
-                if(torches[i].JustChanged())
-                {
-                    UpdateMessageReveal(correctColour);
-                }
-                
-            }
-            else
-            {
-                if(torches[i].JustChanged())
-                {
-                    UpdateMessageReveal(correctColour);
-                }
-                
-            }
+
+
+
+
+
+
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // collided with torch range collider
+        if (other.gameObject.tag == "LightRangeCollider")
+        {
+            // add both torch scripts to lists for later use
+            TorchColor thisTorchColor = other.transform.parent.GetComponent<TorchColor>();
+            TorchSwitch thisTorchSwitch = other.transform.parent.GetComponent<TorchSwitch>();
+
+            torchesColor.Add(thisTorchColor);
+            torchesSwitch.Add(thisTorchSwitch);
+
+            //PrintList(torchesColor);
+            //PrintList(torchesSwitch);
         }
 
-        if (alpha > originalAlpha) alpha -= 0.1f * Time.deltaTime;
-
-
+        
     }
 
-
-    private void UpdateMessageReveal(bool add)
+    private void OnTriggerExit(Collider other)
     {
-        float ratio = ((messageRevealRange - closestLightDistance) / messageRevealRange);
-
-        if (add == true) AddToAlpha(ratio);
-        else SubtractFromAlpha(ratio);
-
-        Color newColor = new Color(message.color.r, message.color.g, message.color.b, alpha);
-
-        message.color = newColor;
-    }
-
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "LightBulletRange")
+        if (other.gameObject.tag == "LightRangeCollider")
         {
-            //Debug.Log("IN RANGE!");
-            float distance = Distance(other.transform.position);
+            // add both torch scripts to lists for later use
+            TorchColor thisTorchColor = other.transform.parent.GetComponent<TorchColor>();
+            TorchSwitch thisTorchSwitch = other.transform.parent.GetComponent<TorchSwitch>();
 
-            if (distance <= messageRevealRange && distance >= 0f)
+            torchesColor.Remove(thisTorchColor);
+            torchesSwitch.Remove(thisTorchSwitch);
+
+            //PrintList(torchesColor);
+            //PrintList(torchesSwitch);
+        }
+    }
+
+
+
+
+    private void RevealFromTorch()
+    {
+        for(int i=0; i < torchesColor.Count; i++)
+        {
+            float torchDistance = Distance(torchesColor[i].transform.position);
+
+            // if torch within distance to reveal message
+            if(torchDistance <= messageRevealRange)
             {
-                    if (other.transform.parent.name == ("LightBullet" + revealWhen.ToString() + "(Clone)"))
-                    {                     
-                        float ratio = ((messageRevealRange - distance) / messageRevealRange);
+                //Debug.Log("In Range!");
+                // if the torch is on
+                if(torchesSwitch[i].GetTorchState())
+                {
+                    //Debug.Log("Torch On!");
+                    // if the torch is the right colour
+                    if (torchesColor[i].GetColorState().Equals(revealWhen.ToString()))
+                    {
+                        //Debug.Log("Correct Colour!");
+                        float distanceRatio = (messageRevealRange - torchDistance) / messageRevealRange;
 
-                        //Debug.Log(ratio);
-
-                        if (ratio > originalAlpha) alpha = ratio;
-                        else alpha = originalAlpha;
-
-                        Color newColor = new Color(message.color.r, message.color.g, message.color.b, alpha);
-
-                        message.color = newColor;
+                        if(alpha <= distanceRatio) alpha += distanceRatio * messageRevealRate * 5f * Time.deltaTime;
                     }
                 }
             }
         }
+    } 
 
 
 
-    private void AddToAlpha(float inNum)
+
+
+
+    private void OnTriggerStay(Collider other)
     {
-        alpha += inNum;
+        // if in the light range of a bullet
+        if(other.gameObject.tag == "LightBulletRange")
+        {
+            // if the bullet is close enough to the message
+            float bulletDistance = Distance(other.transform.parent.position);
 
-        if (alpha >= 1f) alpha = 1f;
-        else if (alpha <= 0f) alpha = 0f;
+            if (bulletDistance <= messageRevealRange)
+            {
+                string messageColor = "LightBullet" + revealWhen.ToString() + "(Clone)";
 
-        originalAlpha = alpha;
+                // if the colour of the bullet is equal to the colour needed to reveal the message
+                if (messageColor.Equals(other.transform.parent.name))
+                {
+                    float distanceRatio = (messageRevealRange - bulletDistance) / messageRevealRange;
 
-        Debug.Log(name + " OG ALPHA: " + originalAlpha);
+                    alpha += distanceRatio * messageRevealRate * 2f * Time.deltaTime;
+                }
+            }
+        }
     }
 
-    private void SubtractFromAlpha(float inNum)
+
+
+
+
+    private void PrintList(List<TorchColor> t)
     {
-        alpha -= inNum;
-
-        if (alpha >= 1f) alpha = 1f;
-        else if (alpha <= 0f) alpha = 0f;
-
-        originalAlpha = alpha;
+        for(int i=0; i < t.Count; i++)
+        {
+            Debug.Log(t[i].name);
+        }
     }
- }
+
+    private void PrintList(List<TorchSwitch> t)
+    {
+        for (int i = 0; i < t.Count; i++)
+        {
+            Debug.Log(t[i].name);
+        }
+    }
+
+
+}
 
 
 
---
+++ Assets/RevealMessage2.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RevealMessage2 : MonoBehaviour
+{
+    private enum ColorState { Red, Orange, Blue, Green, Yellow, Purple, Any};
+
+    [SerializeField]
+    private ColorState revealWhen = ColorState.Any;
+
+    [SerializeField]
+    private float messageRevealRange = 10f;
+
+    private Text message;
+
+    private float closestLightDistance = 1000f;
+
+    private float closestBulletLightDistance = 1000f;
+
+    private float alpha = 0f;
+    private float originalAlpha = 0f;
+
+    private List<TorchColor> torches;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        torches = new List<TorchColor>();
+
+        message = transform.GetChild(0).GetComponent<Text>();
+        alpha = 0f;
+        originalAlpha = alpha;
+
+        Color newColor = new Color(message.color.r, message.color.g, message.color.b, alpha);
+
+        message.color = newColor;
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // collided with torch range collider
+        if(other.gameObject.tag == "LightRangeCollider")
+        {
+            float distance = Distance(other.transform.position);
+
+            // distance is lower than closest torch
+            if(distance < closestLightDistance)
+            {
+                closestLightDistance = distance;
+
+                if (closestLightDistance <= messageRevealRange && closestLightDistance >= 0f)
+                {
+
+                    if(other.transform.parent.GetComponent<TorchSwitch>().GetTorchState())
+                    {
+                        TorchColor thisTorch = other.transform.parent.GetComponent<TorchColor>();
+
+                        torches.Add(thisTorch);
+
+                        if (thisTorch.GetColorState() == revealWhen.ToString())
+                        {
+                            float ratio = ((messageRevealRange - closestLightDistance) / messageRevealRange);
+
+                            AddToAlpha(ratio);
+                        }
+                    }
+
+                    Color newColor = new Color(message.color.r, message.color.g, message.color.b, alpha);
+
+                    message.color = newColor;
+                }
+            }
+
+            
+        }
+    }
+
+
+    private float Distance(Vector3 objectPosition)
+    {
+        float distance;
+
+        distance = (objectPosition - transform.position).magnitude;
+
+        return distance;
+    }
+
+    private Vector3 Direction(Vector3 objectPosition)
+    {
+        Vector3 direction = (objectPosition - transform.position).normalized;
+
+        return direction;
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < torches.Count; i++)
+        {
+            bool correctColour = torches[i].GetColorState() == revealWhen.ToString();
+
+            if (correctColour)
+            {
+                if(torches[i].JustChanged())
+                {
+                    UpdateMessageReveal(correctColour);
+                }
+                
+            }
+            else
+            {
+                if(torches[i].JustChanged())
+                {
+                    UpdateMessageReveal(correctColour);
+                }
+                
+            }
+        }
+
+        if (alpha > originalAlpha) alpha -= 0.1f * Time.deltaTime;
+
+
+    }
+
+
+    private void UpdateMessageReveal(bool add)
+    {
+        float ratio = ((messageRevealRange - closestLightDistance) / messageRevealRange);
+
+        if (add == true) AddToAlpha(ratio);
+        else SubtractFromAlpha(ratio);
+
+        Color newColor = new Color(message.color.r, message.color.g, message.color.b, alpha);
+
+        message.color = newColor;
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "LightBulletRange")
+        {
+            //Debug.Log("IN RANGE!");
+            float distance = Distance(other.transform.position);
+
+            if (distance <= messageRevealRange && distance >= 0f)
+            {
+                    if (other.transform.parent.name == ("LightBullet" + revealWhen.ToString() + "(Clone)"))
+                    {                     
+                        float ratio = ((messageRevealRange - distance) / messageRevealRange);
+
+                        //Debug.Log(ratio);
+
+                        if (ratio > originalAlpha) alpha = ratio;
+                        else alpha = originalAlpha;
+
+                        Color newColor = new Color(message.color.r, message.color.g, message.color.b, alpha);
+
+                        message.color = newColor;
+                    }
+                }
+            }
+        }
+
+
+
+    private void AddToAlpha(float inNum)
+    {
+        alpha += inNum;
+
+        if (alpha >= 1f) alpha = 1f;
+        else if (alpha <= 0f) alpha = 0f;
+
+        originalAlpha = alpha;
+
+        Debug.Log(name + " OG ALPHA: " + originalAlpha);
+    }
+
+    private void SubtractFromAlpha(float inNum)
+    {
+        alpha -= inNum;
+
+        if (alpha >= 1f) alpha = 1f;
+        else if (alpha <= 0f) alpha = 0f;
+
+        originalAlpha = alpha;
+    }
+ }
+
+
+
+
+
